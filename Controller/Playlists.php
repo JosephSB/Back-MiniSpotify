acb266daaf117a46a2499198f05e34d554fdcd51
JosephSB <jguku2002@gmail.com>
2021-11-27 18:39:19 -0500

    v1.2.2 agregue para poder añadir canciones a la playlist

--- Controller/Playlists.php
+++ Controller/Playlists.php
@@ -18,11 +18,30 @@
                         if($this->model->addPlaylist($data)){
 
                             echo $this->sendJson('Playlist creada exitosamente', true);
-                            
+
                         }else echo $this->sendJson('Ocurrio un error al crear la playlist', false);
 
                     }else echo $this->sendJson('Datos vacios', false);
                 }else echo $this->sendJson('El usuario no existe', false);
+            }else echo $this->sendJson('Datos Incompletos', false);
+        }
+
+        public function addSongPlalist()
+        {
+            $data = json_decode(file_get_contents('php://input'), true);
+
+            if(count($data)==2){
+
+                if($this->model->existSongID($data['IDsong'])&&
+                $this->model->existPlaylistID($data['IDplaylist'])
+                ){
+
+                    if($this->model->addSong_Playlist($data)){
+                        echo $this->sendJson('Cancion agregada correctamente', true);
+                    }else echo $this->sendJson('Error al guardar la cancion', false);
+                    
+                }else  echo $this->sendJson('Los datos no existen', false);
+
             }else echo $this->sendJson('Datos Incompletos', false);
         }
 
--- Model/PlaylistsModel.php
+++ Model/PlaylistsModel.php
@@ -30,6 +30,25 @@
             }
         }
 
+        public function addSong_Playlist($data)
+        {
+            try {
+                $query = $this->db->connect()->prepare(
+                    'INSERT INTO playlist_song VALUES (:idPlaylist, :idsong)'
+                );
+
+                $query->execute([
+                    'idPlaylist'=>$data['IDplaylist'],//generar id
+                    'idsong'=>$data['IDsong']
+                ]);
+                return true;
+
+            } catch (PDOException $e) {
+                echo $e;
+                return false;
+            }
+        }
+
         public function addAutorPlaylist($idautor,$idplaylist)
         {
             try {
@@ -42,6 +61,42 @@
                     'iduser'=>$idautor
                 ]);
                 return true;
+            } catch (PDOException $e) {
+                echo $e;
+                return false;
+            }
+        }
+
+        public function existSongID($idsong)
+        {
+            try {
+                $query = $this->db->connect()->prepare(
+                    'SELECT ID_SONG,SONGNAME FROM song WHERE ID_SONG = :idsong'
+                );
+
+                $query->execute(['idsong'=>$idsong]);
+                while($query->fetch()){
+                    return true;
+                }
+                return false;
+            } catch (PDOException $e) {
+                echo $e;
+                return false;
+            }
+        }
+
+        public function existPlaylistID($idplaylist)
+        {
+            try {
+                $query = $this->db->connect()->prepare(
+                    'SELECT ID_PLAYLIST FROM playlist WHERE ID_PLAYLIST = :idplaylist'
+                );
+
+                $query->execute(['idplaylist'=>$idplaylist]);
+                while($query->fetch()){
+                    return true;
+                }
+                return false;
             } catch (PDOException $e) {
                 echo $e;
                 return false;
